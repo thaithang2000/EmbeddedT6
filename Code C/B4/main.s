31366f8d852a401b2c9eafcaae7c813b25e3c616
thaithang2000 <136157839+thaithang2000@users.noreply.github.com>
2023-06-12 19:46:28 +0700

    Add files via upload

---
+++ Code C/B4/main.s
@@ -0,0 +1,82 @@
+	.file	"main.c"
+	.section .rdata,"dr"
+LC0:
+	.ascii "This is test \0"
+	.text
+	.globl	_test
+	.def	_test;	.scl	2;	.type	32;	.endef
+_test:
+LFB10:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	subl	$24, %esp
+	movl	$LC0, (%esp)
+	call	_puts
+	nop
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE10:
+	.section .rdata,"dr"
+LC1:
+	.ascii "Hello world\0"
+	.text
+	.globl	_display
+	.def	_display;	.scl	2;	.type	32;	.endef
+_display:
+LFB11:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	subl	$24, %esp
+	movl	$LC1, (%esp)
+	call	_puts
+	nop
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE11:
+	.def	___main;	.scl	2;	.type	32;	.endef
+	.section .rdata,"dr"
+LC2:
+	.ascii "MAX: %d\12\0"
+	.text
+	.globl	_main
+	.def	_main;	.scl	2;	.type	32;	.endef
+_main:
+LFB12:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	andl	$-16, %esp
+	subl	$16, %esp
+	call	___main
+	call	_display
+	movl	$10, 4(%esp)
+	movl	$LC2, (%esp)
+	call	_printf
+	call	_test
+	movl	$0, %eax
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE12:
+	.ident	"GCC: (MinGW.org GCC-6.3.0-1) 6.3.0"
+	.def	_puts;	.scl	2;	.type	32;	.endef
+	.def	_printf;	.scl	2;	.type	32;	.endef
