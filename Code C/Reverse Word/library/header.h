fe48a74e28d70e6205766671ee24e8a912cf56bf
thaithang2000 <136157839+thaithang2000@users.noreply.github.com>
2023-06-29 18:15:16 +0700

    Add files via upload

---
+++ Code C/Reverse Word/library/header.h
@@ -0,0 +1,16 @@
+/*
+* File: sample_file.c
+* Author: Thai Thang
+* Date: 29/06/2023
+* Description: This is a sample file for Reverse string
+*/
+#include "source.c"
+
+// Find length of string
+int length(char *str);
+
+// Reverse string
+void reversestring(char *arr, int start, int end);
+
+// Reversw word in string
+void reverseword(char *arr);
